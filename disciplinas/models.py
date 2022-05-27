06786bbf466e691ab6a89232064450ab0a194c01
gabrielonishi <gabrielonishi@outlook.com>
2022-05-27 18:47:43 -0300

    Adiciona endpoint de upload de resumo com autenticação

--- disciplinas/models.py
+++ disciplinas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -27,6 +28,7 @@ class Summary(models.Model):
     filename = models.CharField(max_length=50)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     file = models.FileField(upload_to='resumos/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.filename
--- disciplinas/views.py
+++ disciplinas/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, Http404, HttpResponseForbidden, JsonResponse
 
-from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Course, Subject, Summary
 from .serializers import CourseSerializer, SubjectSerializer, SummarySerializer
@@ -31,7 +34,7 @@ def api_subjects(request, course):
     serialized_subjects = SubjectSerializer(subjects_list, many=True)
     return Response(serialized_subjects.data)
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def api_summarys(request, course, subject):
     try:
         course_obj = Course.objects.get(url_course_name=course)
@@ -42,3 +45,31 @@ def api_summarys(request, course, subject):
     
     serialized_summarys = SummarySerializer(summary_list, many=True)
     return Response(serialized_summarys.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def api_post_summary(request, course, subject):
+    try:
+        course_obj = Course.objects.get(url_course_name=course)
+        subject_obj = Subject.objects.get(url_subject_name=subject)
+
+    except Course.DoesNotExist or Subject.DoesNotExist:
+        raise Http404()
+    
+    return HttpResponse('oi!')
+    
+@api_view(['POST'])
+def api_get_token(request):
+    try:
+        if request.method == 'POST':
+            username = request.data['username']
+            password = request.data['password']
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                token, created = Token.objects.get_or_create(user=user)
+                return JsonResponse({"token":token.key})
+            else:
+                return HttpResponseForbidden()
+    except:
+        return HttpResponseForbidden()
