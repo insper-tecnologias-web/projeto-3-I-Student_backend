36fac0ae13104794e688bd2c7bf88c11021bdf21
gabrielonishi <gabrielonishi@outlook.com>
2022-06-15 17:28:04 -0300

    Add dj_database_url

--- istudent/settings.py
+++ istudent/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,15 +93,23 @@ WSGI_APPLICATION = 'istudent.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'istudent',
+#         'USER': 'istudentuser',
+#         'PASSWORD': 'istudentsenha',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'istudent',
-        'USER': 'istudentuser',
-        'PASSWORD': 'istudentsenha',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://localhost/istudent?user=istudentuser&password=istudentsenha',
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
 }
 
 # Password validation
